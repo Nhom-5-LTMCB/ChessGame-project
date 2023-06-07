5c65e54b120578798bb11e6fe1b76fa61706a181
longle1 <ltphilong2001@gmail.com>
2023-06-07 07:26:02 +0700

    update file bao cao

--- ChessGame-Api/app/models/auth.js
+++ ChessGame-Api/app/models/auth.js
@@ -77,7 +77,7 @@ module.exports = {
         return user;
     },
     createUser: async (body) => {
-        let user = await usersModel.findOne({ userName: body.userName, gmail: body.gmail });
+        let user = await usersModel.findOne({ userName: body.userName });
         if (user) {
             return false;
         }
